7c23c986aec28a9ecc8c5b3dd90244929c14210a
Michael Bowlin <jaidoutani@gmail.com>
2022-10-05 18:40:51 -0500

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // console.log(`Item name: ${name}`)
+  // console.log(`Item category: ${category}`)
+  const [inCart, setInCart] = useState(false);
+  function handleClick() {
+    setInCart(!inCart);
+  }
+
   return (
-    <li className="">
+    <li className="in-cart">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">
+        {inCart ? "Add to Cart" : "Remove from cart"}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  //console.log(items)
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleFilterChange(e) {
+    //console.log(e.target.value)
+    // console.log(selectedCategory)
+    if(selectedCategory === e.target.value) {
+      return setSelectedCategory(selectedCategory)
+    }
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
